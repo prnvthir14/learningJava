1036db501bb46841dccd85baea07c63f30e78449
pranav <prnvthir@gmail.com>
2021-05-10 22:50:52 -0400

    add divide, mod methods

--- Calculator.java
+++ Calculator.java
@@ -1,23 +1,47 @@
 public class Calculator {
+  public int mod(int a, int b) {
 
+    int modResult = a % b;
 
+    return modResult;
 
-  public int add (int a, int b) {
-
-    int result = a +b;
-
-    return result;
   }
 
 
-  public Calculator (int a, int b) {
+  public int divide(int a, int b) {
 
+    int divResult = a / b;
 
-  } 
+    return divResult;
 
-  public static void main (String[] args){
+  }
 
-    
+  public int product(int a, int b) {
+
+    int multResult = a * b;
+
+    return multResult;
+
+  }
+
+  public int subtract(int a, int b) {
+    int subResult = a - b;
+
+    return subResult;
+  }
+
+  public int add(int a, int b) {
+
+    int addResult = a + b;
+
+    return addResult;
+  }
+
+  public Calculator(int a, int b) {
+
+  }
+
+  public static void main(String[] args) {
 
   }
 
