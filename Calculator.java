d267cdbb7cbd12ed796e020fd3e1ffc205b08a0d
pranav <prnvthir@gmail.com>
2021-05-10 22:43:44 -0400

    add add method

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+
+
+
+  public int add (int a, int b) {
+
+    int result = a +b;
+
+    return result;
+  }
+
+
+  public Calculator (int a, int b) {
+
+
+  } 
+
+  public static void main (String[] args){
+
+    
+
+  }
+
+}
