fb7ec51a91b7647a4d25e5df333c2064e8090244
pranav <prnvthir@gmail.com>
2021-05-12 14:53:06 -0400

    add onlyPrimes method to add prime numbers to return ArrayList

--- PrimeDirective.java
+++ PrimeDirective.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+
 //build a program that creates an ArrayList of all prime numbers in an array.
 
 public class PrimeDirective {
+
+  public ArrayList<Integer> onlyPrimes(int[] numbers) {
+
+    ArrayList<Integer> primes = new ArrayList<Integer>();
+    for (int number : numbers) {
+      if (isPrime(number)) {
+        // add number to primes
+        primes.add(number);
+      }
+    }
+    return primes;
+  }
 
   // method to check if a num is prime
   public boolean isPrime(int number) {
@@ -12,7 +26,8 @@ public class PrimeDirective {
     } else if (number < 2) {
       return false;
     }
-
+    // f n is prime, then n should not be divisible by any integers between 2 and
+    // n-1.
     for (int i = 2; i < number; i++) {
 
       if (number % i == 0) {
@@ -26,10 +41,12 @@ public class PrimeDirective {
   public static void main(String[] args) {
 
     PrimeDirective pd = new PrimeDirective();
-    System.out.println(pd.isPrime(7));
-    System.out.println(pd.isPrime(28));
-    System.out.println(pd.isPrime(2));
-    System.out.println(pd.isPrime(0));
+    // System.out.println(pd.isPrime(7));
+    // System.out.println(pd.isPrime(28));
+    // System.out.println(pd.isPrime(2));
+    // System.out.println(pd.isPrime(0));
+
+    int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
 
   }
 }
