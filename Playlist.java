ab65065169d5d3827dc5a45747d86ad72e0f2583
pranav <prnvthir@gmail.com>
2021-05-12 13:09:58 -0400

    add main method with ArrayList decl and inserted data

---
+++ Playlist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Playlist {
+
+  public static void main(String[] args) {
+    // create new arrayList;
+    ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+    desertIslandPlaylist.add("a");
+    desertIslandPlaylist.add("b");
+    desertIslandPlaylist.add("c");
+    desertIslandPlaylist.add("d");
+    desertIslandPlaylist.add("e");
+    System.out.println(desertIslandPlaylist);
+
+  }
+
+}
